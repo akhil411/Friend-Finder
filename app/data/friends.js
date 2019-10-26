5298cb33a390702a97cf6cec4e716648295c4c4f
Akhil <akhilvijayan004@gmail.com>
2019-10-26 22:44:08 +1100

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,118 @@
+
+  var friendsArray = [
+      {
+        "name":"Iron Man",
+            "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTILCLDmhVIVzC3_b8pyQO4Z8uek_vVFiGlIR2OvfJYnmUtOJ7WMGusrlIC&s",
+            "scores":[
+                5,
+                1,
+                4,
+                4,
+                5,
+                1,
+                2,
+                5,
+                4,
+                1
+              ]
+      },
+      {
+        "name":"Captain America",
+            "photo":"https://i.imgur.com/G14V7Sl.jpg",
+            "scores":[
+                4,
+                2,
+                5,
+                5,
+                3,
+                2,
+                1,
+                1,
+                5,
+                4
+              ]
+      },
+      {
+        "name":"Hulk",
+            "photo":"https://vignette.wikia.nocookie.net/marvelvscapcom/images/5/5d/HulkBruce.png/revision/latest?cb=20170818123736",
+            "scores":[
+                3,
+                2,
+                5,
+                5,
+                4,
+                2,
+                3,
+                1,
+                5,
+                2
+              ]
+      },
+      {
+        "name":"Ant Man",
+            "photo":"https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/798:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg",
+            "scores":[
+                4,
+                5,
+                3,
+                3,
+                4,
+                5,
+                1,
+                4,
+                3,
+                5
+              ]
+      },
+      {
+        "name":"Spider Man",
+            "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTILCLDmhVIVzC3_b8pyQO4Z8uek_vVFiGlIR2OvfJYnmUtOJ7WMGusrlIC&s",
+            "scores":[
+                3,
+                3,
+                2,
+                2,
+                3,
+                5,
+                4,
+                3,
+                2,
+                5
+              ]
+      },
+      {
+        "name":"Doctor Strange",
+            "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTILCLDmhVIVzC3_b8pyQO4Z8uek_vVFiGlIR2OvfJYnmUtOJ7WMGusrlIC&s",
+            "scores":[
+                5,
+                1,
+                4,
+                4,
+                5,
+                1,
+                2,
+                5,
+                4,
+                1
+              ]
+      },
+      {
+        "name":"Black Panther",
+            "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTILCLDmhVIVzC3_b8pyQO4Z8uek_vVFiGlIR2OvfJYnmUtOJ7WMGusrlIC&s",
+            "scores":[
+                4,
+                2,
+                5,
+                3,
+                1,
+                5,
+                4,
+                5,
+                1,
+                3
+              ]
+      },
+      
+    ];
+
+module.express = friendsArray;
